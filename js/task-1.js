60f58d7e465bdc2d493699a6117fc3dc96f55603
Alina Kikh <akar@i.ua>
2024-12-09 10:26:00 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,9 @@
 function makeTransaction(quantity, pricePerDroid) {
-    const product = "You ordered " + quantity + " droids worth " + pricePerDroid + " credits!";
+    const totalPrice = quantity * pricePerDroid;
+    const product = "You ordered " + quantity + " droids worth " + totalPrice + " credits!";
     return product;
 }
+
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
